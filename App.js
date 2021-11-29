b8e8d731b496b0d44dc61661aa7aebc6ecf61d5c
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-29 11:16:27 -0600

    restructured folders

--- App.js
+++ App.js
@@ -4,17 +4,15 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Start from './components/start';
-import Chat from './components/chat';
+import Start from './src/components/start';
+import Chat from './src/components/chat';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
+      <Stack.Navigator initialRouteName="Start">
         <Stack.Screen
           name="Start"
           component={Start}
--- src/components/chat.js
+++ src/components/chat.js
@@ -6,6 +6,7 @@ export default class Chat extends React.Component {
     render() {
         let { username } = this.props.route.params;
         this.props.navigation.setOptions({ title: username })
+
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>See Chats Here!</Text>
