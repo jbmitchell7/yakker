88aea23c6ef7a5d908d3b60b55fd99a6081c7d02
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-29 12:17:26 -0600

    added initial color selector

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 export default class Chat extends React.Component {
     render() {
         let { username } = this.props.route.params;
         this.props.navigation.setOptions({ title: username })
-
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={styles.container}>
                 <Text>See Chats Here!</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+});
--- src/components/start.js
+++ src/components/start.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, TextInput, Button } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Start extends React.Component {
     constructor(props) {
@@ -9,20 +10,32 @@ export default class Start extends React.Component {
 
     render() {
         return (
-            <ScrollView style={styles.container}>
+            <View style={styles.container}>
                 <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    style={styles.nameInput}
                     onChangeText={(username) => this.setState({ username })}
                     value={this.state.username}
                     placeholder='Enter Name Here'
                 />
+                <View>
+                    <TouchableOpacity
+                        accessible={true}
+                        accessibilityLabel="choose green background"
+                        accessibilityHint="choose chat screen background"
+                        accessibilityRole="button"
+                        onPress={() => { }}
+                    >
+                        <View style={styles.color1}></View>
+                    </TouchableOpacity>
+                </View>
                 <Button
+                    style={styles.btn}
+                    title="Go to Chat"
                     onPress={() => {
                         this.props.navigation.navigate('Chat', { username: this.state.username })
                     }}
-                    title="Go to Chat"
                 />
-            </ScrollView>
+            </View>
         );
     }
 }
@@ -30,6 +43,27 @@ export default class Start extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#0DD5B2'
+        backgroundColor: '#0DD5B2',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    nameInput: {
+        height: 40,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        textAlign: 'center',
+        marginBottom: 10
+    },
+    btn: {
+        backgroundColor: '#FECB7A',
+        borderRadius: 5
+    },
+    color1: {
+        width: 40,
+        height: 40,
+        borderRadius: 40,
+        backgroundColor: 'green'
     }
 });
