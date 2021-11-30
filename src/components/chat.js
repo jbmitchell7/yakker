a080c85dfd3a8dae26d2ea3acc0f882dce765a8d
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-30 12:52:38 -0600

    changed route name and added semicolons

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,10 +4,10 @@ import { View, Text, StyleSheet } from 'react-native';
 export default class Chat extends React.Component {
     componentDidMount() {
         let { username } = this.props.route.params;
-        this.props.navigation.setOptions({ title: username })
+        this.props.navigation.setOptions({ title: username });
     }
     render() {
-        let { color } = this.props.route.params
+        let { color } = this.props.route.params;
         return (
             <View style={{ flex: 1, backgroundColor: color ? color : 'black' }}>
                 <Text style={{ color: 'yellow' }}>See Chats Here!</Text>
