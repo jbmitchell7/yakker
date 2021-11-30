aba9207d69753392ea73607c27308e139aa642fe
jbmitchell7 <jbmitchell747@gmail.com>
2021-11-30 12:48:49 -0600

    fixed warning in chat file

--- src/components/chat.js
+++ src/components/chat.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default class Chat extends React.Component {
-    render() {
-        let { username, color } = this.props.route.params;
+    componentDidMount() {
+        let { username } = this.props.route.params;
         this.props.navigation.setOptions({ title: username })
+    }
+    render() {
+        let { color } = this.props.route.params
         return (
             <View style={{ flex: 1, backgroundColor: color ? color : 'black' }}>
                 <Text style={{ color: 'yellow' }}>See Chats Here!</Text>
